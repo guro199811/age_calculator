7086ac033a17399026d9845ea55ddde38ecb8239
guram sivsivadze <guro.sivs@gmail.com>
2025-04-13 15:27:48 +0400

    feat: add static folder, add styling to web

--- backend/app.py
+++ backend/app.py
@@ -11,8 +11,15 @@ def create_app():
     Creates flask app, configures it and initializes the database
     :return: Flask app
     """
-    app = Flask(__name__.split(".")[0])
-    app.template_folder = absolute_path(__file__, "../frontend")
+    app = Flask(__name__)
+
+    # Configure template and static folders with absolute paths
+    frontend_path = absolute_path(__file__, "../frontend")
+    static_path = absolute_path(__file__, "../frontend/static")
+    app.template_folder = frontend_path
+    app.static_folder = static_path
+    app.static_url_path = ""
+
     database_path = absolute_path(__file__, "database/history.db")
     app.config["SQLALCHEMY_DATABASE_URI"] = f"sqlite:///{database_path}"
     # Adjusting Cors to accept all origins
@@ -20,6 +27,7 @@ def create_app():
     app.config["CORS_ORIGINS"] = "*"
     app.config["CORS_METHODS"] = "GET, POST, OPTIONS"
     app.config["CORS_ALLOW_CREDENTIALS"] = True
+    app.config["DEBUG"] = True
 
     db.init_app(app)
     with app.app_context():
